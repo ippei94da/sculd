18870875e2f8d6da77a806b80333c3940383c9a9
ippei94da <ippei94da@gmail.com>
2012-10-23 08:51:09 +0900

    small revision

--- spec/plan_spec.rb
+++ spec/plan_spec.rb
@@ -1,4 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Norn" do
+describe Plan do # E.g., Klass
+  context 'Class method' do # 'when stack is empty'
+    describe '#parse' do # ''
+      str = '[2012-10-23]! deadlineA'
+      context str do
+        it 'should return Date, !, deadlineA' do
+          a, b, c = Plan.parse(str)
+          a.should == Date.new(2012, 10, 23)
+          b.should == "!"
+          c.should == "deadlineA"
+        end
+      end
+    end
+  end
 end
+
